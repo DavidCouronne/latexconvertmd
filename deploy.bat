b42c123e179db626a2107ff728822bf3cd412630
DavidCouronne <couronne.david@gmail.com>
2019-01-04 18:58:59 +0100

    update version

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,3 @@
 python setup.py sdist bdist_wheel
 twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+python -m pip install --index-url https://test.pypi.org/simple/ latexconvertmd --upgrade
