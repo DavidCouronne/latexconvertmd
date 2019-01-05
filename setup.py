889a9e2c10ad1b922aee77ef2c1b560ffdbcfa16
DavidCouronne <couronne.david@gmail.com>
2019-01-05 19:16:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="latexconvertmd",
-    version="0.0.5",
+    version="0.0.6",
     author="David Couronné",
     author_email="couronne.david@gmail.com",
     description="Convertion LaTeX en Markdown",
