12ee2804e8a92b21457002d501edbde5ebedc411
DavidCouronne <couronne.david@gmail.com>
2019-01-05 12:29:35 +0100

    Create config.js

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,61 @@
+module.exports = {
+    head: [
+      
+      ['link', {rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons"}],
+      ['link', {rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css"}],
+    ],
+    
+  
+  
+    themeConfig: {
+      repo: 'DavidCouronne/mathssyfy',
+      docsDir: 'mathssyfy',
+      editLinks: true,
+      editLinkText: 'Edit this page on GitHub',
+      
+      locales: {
+        '/': {
+          label: 'Français',
+          selectText: 'Languages',
+          lastUpdated: 'Last Updated'
+        }},
+      sidebarDepth: 3,
+      nav: [
+        { text: 'Accueil', link: '/' },
+        { text: 'Guide', link: '/docs/guide/' },  
+      ],
+      sidebar: {
+        '/docs/maths/sujets/2018-nvellecaledonie/': [
+          '',
+          'corrige',
+        ],
+        '/docs/informatique/Vuepress/': [
+          '',
+          'demarrer',
+          'config1',
+          'config2',
+          'sidebar',
+          'exercice',
+          'katex',
+          'deploy',
+          
+        ],
+        '/docs/informatique/HowTo/': [
+          ''
+        ],
+        '/docs/': [
+          '',
+        ],
+      },
+      
+    },
+    
+    /* markdown: {
+      lineNumbers: false,
+      config: md => {
+        var mf = require('markdown-it-footnote');
+        md.use(require('./param-katex'));
+        md.use(mf);
+      }
+    } */
+  }
